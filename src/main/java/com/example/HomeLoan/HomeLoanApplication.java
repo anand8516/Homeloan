ada84a75cdb9e099245ac141cd4918ef3b6da524
Anandk <vedantk1508@gmail.com>
2022-07-25 18:34:28 +0530

    Initial commit

--- src/main/java/com/example/HomeLoan/HomeLoanApplication.java
+++ src/main/java/com/example/HomeLoan/HomeLoanApplication.java
@@ -1,12 +1,16 @@
 package com.example.HomeLoan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class HomeLoanApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello World");
 		SpringApplication.run(HomeLoanApplication.class, args);
 	}
 
