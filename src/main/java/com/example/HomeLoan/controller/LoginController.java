74775812939f26405b953b839ede85bfa220a187
Nikhil Mahajan <mahajannikhil609@gmail.com>
2022-07-26 09:22:39 +0530

    login

--- src/main/java/com/example/HomeLoan/controller/LoginController.java
+++ src/main/java/com/example/HomeLoan/controller/LoginController.java
@@ -1,20 +1,41 @@
 package com.example.HomeLoan.controller;
 
 import java.util.List;
+import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.HomeLoan.model.Users;
+import com.example.HomeLoan.service.UserService;
 
 
 
-@Controller
+@RestController
 public class LoginController {
+	@Autowired	
+	UserService userService;
+	
+	@PostMapping(value="api/create/user")
+	public Users createUser(@RequestBody Users user) {
+		
+		return userService.createUser(user);
+	}
+		
+	
+	@GetMapping(value = "api/getuser/{userId}")
+	public Optional<Users> getUser(@PathVariable int userId) {
+		
+		return userService.getUser(userId);
+	}
+
 
 }
--- src/main/java/com/example/HomeLoan/model/Users.java
+++ src/main/java/com/example/HomeLoan/model/Users.java
@@ -5,25 +5,27 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-
+import lombok.Setter;
 
 @Data
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Users {
 	
 	@Id
 	@Column(name = "user_id")
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer userId;	
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private int userId;	
 	
 	@Column(name="user_password")
 	private String password;
@@ -32,7 +34,40 @@ public class Users {
 	private String email;
 	
 	@Column(name="user_phone")
-	private Long phone;
+	private long phone;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
 	
 	
 }
