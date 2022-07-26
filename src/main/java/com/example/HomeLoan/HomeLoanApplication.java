f768f6eb99d9adda7a2cc8467862c7e9a5f1cd3b
unknown <piyushjha65@gmail.com>
2022-07-26 23:05:04 +0530

    added mail service, global exception handling and swagger-ui

--- src/main/java/com/example/HomeLoan/HomeLoanApplication.java
+++ src/main/java/com/example/HomeLoan/HomeLoanApplication.java
@@ -4,14 +4,14 @@ package com.example.HomeLoan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-import com.example.HomeLoan.service.LoanRepaymentService;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@EnableSwagger2
 public class HomeLoanApplication {
 	
 	
@@ -20,9 +20,7 @@ public class HomeLoanApplication {
 							
         logger.info("Info log InSIde Home APP");
 		SpringApplication.run(HomeLoanApplication.class, args);
-		
-		LoanRepaymentService obj =  new LoanRepaymentService();
-		obj.generateRepaymentSchedule(new Date(),5000000, 7,20,0);
+
 
 		
 	}
