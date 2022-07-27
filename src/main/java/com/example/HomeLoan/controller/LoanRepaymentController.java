9410ccfc62b8729b29840278d282fdf1b6a212fe
Anandk <vedantk1508@gmail.com>
2022-07-27 11:36:01 +0530

    Prepayment and paymentAPIs

--- src/main/java/com/example/HomeLoan/controller/LoanRepaymentController.java
+++ src/main/java/com/example/HomeLoan/controller/LoanRepaymentController.java
@@ -3,18 +3,56 @@ package com.example.HomeLoan.controller;
 
 import java.util.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import com.example.HomeLoan.service.LoanAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.HomeLoan.model.LoanAccount;
 import com.example.HomeLoan.service.LoanRepaymentService;
+
+@RestController
 public class LoanRepaymentController {
+	@Autowired
 	private LoanRepaymentService loanservice;
+
+	@Autowired
+	private LoanAccountService loanAccountService;
 	
 	
 	@GetMapping("/loanscheduler/{id}/")
 	public LoanAccount findLoandetaisbyID(@PathVariable int Id) {
 		return loanservice.getLoanAccountById(Id);
+	}
+
+	@PostMapping("/prepayment/{loanaccountno}")
+	public ResponseEntity<? >  updateEMIDetails(@PathVariable int loanaccountno,@RequestBody Double partialPaymentAmount) {
+
+
+		try {
+
+			return new ResponseEntity<>( loanservice.updateEMIDetails(loanaccountno,partialPaymentAmount), HttpStatus.OK);
+		}
+		catch(Exception e){
+
+			return new ResponseEntity<>("Error occurred during prepayment", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@PutMapping("/emipayment/{loanaccountno}")
+	public ResponseEntity<?> updateMonthlyEMIPayment(@PathVariable int loanaccountno)
+	{
+		try {
+
+			return new ResponseEntity<>( loanservice.emiPayment(loanaccountno), HttpStatus.OK);
+		}
+		catch(Exception e){
+
+			return new ResponseEntity<>("Error occurred during prepayment", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 	
 }
--- src/main/java/com/example/HomeLoan/service/LoanRepaymentService.java
+++ src/main/java/com/example/HomeLoan/service/LoanRepaymentService.java
@@ -7,9 +7,13 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.example.HomeLoan.model.SavingAccount;
+import com.example.HomeLoan.repo.SavingAccountRepositiory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 
@@ -22,6 +26,14 @@ public class LoanRepaymentService {
 	private static final Logger logger = LogManager.getLogger(LoanRepaymentService.class);
 	@Autowired
 	private LoanAccountRepository repo;
+
+	@Autowired
+	private SavingAccountRepositiory savingAccRepo;
+
+
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	
 	@Autowired
 	private utility dateUtility;
@@ -60,13 +72,13 @@ public class LoanRepaymentService {
 		LoanRepaymentService pesObj = new LoanRepaymentService();
 		logger.info("Entere generateRepaymentSchedule");
 		double p = principleAmount;
-		double r = interestRate/12;
+		double r = interestRate/1200;
 		double t = (year * 12) + month;
 		double emi = pesObj.calMonthlyEmi(p,r,t);		//emi = (p*r*Math.pow(1+r,t))/(Math.pow(1+r,t)-1);
 		double outstanding = p;				
 
 		
-		for(int i=0;i<t;i++){
+		for(int i=0;i<t+1;i++){
 			double mInterest = pesObj.calInterest(outstanding,r) ;//rate is monthly //= outstanding * r;			
 			double paidPrinciple = pesObj.calPaidPrinciple(emi,mInterest);//emi - mInterest;			
 			outstanding = outstanding - paidPrinciple;				
@@ -76,10 +88,9 @@ public class LoanRepaymentService {
 			obj.setOutstanding(outstanding);
 			obj.setInterest(mInterest);			
 			obj.setPrinciple(paidPrinciple);
-<<<<<<< HEAD
-=======
+
 			obj.setPrinciple(paidPrinciple);
->>>>>>> a6e08f3c335c9e900ecfed9fcd692eb5da478a9f
+
 			obj.setEmi(emi);
 			
 			
@@ -92,8 +103,87 @@ public class LoanRepaymentService {
 			}				
 			return repaySchedule;
 		}
-	
-	
+	public String updateEMIDetails(int loanaccountno,Double amount)
+	{
+		LoanRepaymentService pesObj = new LoanRepaymentService();
+
+		LocalDate currentDate= LocalDate.now();
+	int paidmonthscount=jdbcTemplate.queryForObject("select COUNT(*) from repayment where loan_account_id=? and status='paid'",new Object[]{loanaccountno},Integer.class);
+	logger.info("count"+paidmonthscount);
+	logger.info("date"+currentDate);
+	if(paidmonthscount>=3) {
+
+		String currentmonthemi = "select date,interest,principle,outstanding,emi from repayment where loan_account_id = ?  and status='paid' and date <= ? order by date DESC limit 1";
+		Repayment repayment = (Repayment) jdbcTemplate.queryForObject(currentmonthemi, new Object[]{loanaccountno, currentDate},
+				new BeanPropertyRowMapper(Repayment.class));
+
+		System.out.println(repayment);
+		Date emiDate = repayment.getDate();
+		double outstanding = repayment.getOutstanding();
+		double rate = 7.0 / 1200;
+		int tenuremonths = repo.findByLoanAccId(loanaccountno).getYear() * 12 + repo.findByLoanAccId(loanaccountno).getMonth();
+		double balance = outstanding - amount;//prepayment done
+		int pendingmonths = tenuremonths - paidmonthscount;
+		double newemi = (balance * rate * Math.pow(1 + rate, pendingmonths)) / (Math.pow(1 + rate, pendingmonths) - 1);
+		for (int i = 0; i < pendingmonths + 1; i++) {
+			double updatedInterest = balance * rate;
+			double updatedPrinciple = newemi - updatedInterest;
+			balance = balance - updatedPrinciple;
+			System.out.println("emi:" + newemi + "interest:" + updatedInterest + "monthlyprinciple" + updatedPrinciple);
+
+			int monthly_inc = 1;
+			Date new_date = pesObj.addMonths(emiDate, monthly_inc + i);
+			logger.info("new date:" + new_date);
+
+			String updateSQL = "update repayment set emi=?,interest=?,outstanding=?,principle=? where date=? and loan_account_id=? and status!='paid'";
+			jdbcTemplate.update(updateSQL, newemi, updatedInterest, balance, updatedPrinciple, new_date, loanaccountno);
+		}
+
+		return "prepaymentdone,outstanding balance"+(outstanding-amount);
+	}
+	else
+	{
+		return "Cannot prepay ";
+	}
+
+
+
+	}
+
+	public String emiPayment(int loanaccountno)
+	{
+
+			LoanAccount loanAccount=repo.findByLoanAccId(loanaccountno);
+			long saving_acc_no=loanAccount.getAccountNo();
+			
+			logger.info("saving accountno:"+saving_acc_no);
+		SavingAccount userAccount = savingAccRepo.findByAccountno(saving_acc_no);
+		logger.info("useraccount"+userAccount);
+			Double currentBalance=userAccount.getBalance();
+
+
+		String currentmonthemi="select id as repaymentid,date,interest,principle,outstanding,emi from repayment where loan_account_id = ?  and status='pending' order by date ASC limit 1";
+		Repayment repayment=(Repayment)jdbcTemplate.queryForObject(currentmonthemi,new Object[]{  loanaccountno },
+				new BeanPropertyRowMapper(Repayment.class));
+		logger.info("repayment"+repayment);
+		if(currentBalance>repayment.getEmi()) {
+			Double balanceafterDeduction = currentBalance - repayment.getEmi();
+			int repayId = repayment.getRepaymentid();
+
+			jdbcTemplate.update("update repayment set status='paid' where loan_account_id=? and id=?", loanaccountno, repayId);
+			jdbcTemplate.update("update savingaccount set curr_balance=? where acc_no=?", balanceafterDeduction, saving_acc_no);
+			logger.info("EMI deducted");
+			return "EMI deduct success";
+		}
+		else
+		{
+			return "not enough balance";
+		}
+
+
+	}
+
+
 }
 
 
